4c5bc90d91b0a1ac23440fab559d1554dda8d35c
Annie Truex <artruex@gmail.com>
2019-07-26 16:38:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ") #turn string into an array
   new_array = []
   array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
+    new_array << string.reverse #reverse each word in the array
   end
   new_array.join(" ")
 end
